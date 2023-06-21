0dec0e0405f21a9d9391b92b57f4aca6400e65e6
Dharvik19 <dharvik.badga22@gmail.com>
2023-06-21 18:03:05 +0530

    added the nested routes to developer page

---
+++ next-js-tutorial/pages/about-us/developer.js
@@ -0,0 +1,6 @@
+function Developer(){
+    return (
+    <h1>Dharvik here.</h1>
+    )
+}
+export default Developer;
---
+++ next-js-tutorial/pages/news/something-important.js
@@ -0,0 +1,8 @@
+//our-domain.com/news/something-important
+
+function DetsilPage(){
+    return (
+    <h1>The Detail Page</h1>
+    )
+}
+export default DetsilPage;
